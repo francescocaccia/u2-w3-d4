c8d36f55f63260c292ddf6bbea44e9e54e0a0a8d
francescocaccia <123968277+francescocaccia@users.noreply.github.com>
2023-03-15 16:47:31 +0100

    homeWork

--- script.js
+++ script.js
@@ -15,16 +15,23 @@ fetch("https://striveschool-api.herokuapp.com/books")
                             <h5 class="card-title">${book.title}</h5>
                             <img class="w-100" src=${book.img}>
                             <span class="badge bg-dark">${book.category}</span >
-                            <p class="card-text">€ ${book.price}</p>
+                            <p class="card-text">Price € ${book.price}</p>
+                            <button class="bottoneCanc">🗑</button>
                         </div>
                     </div>
                         `;
       grid.appendChild(col);
-      const btn = document.createElement("button");
+      /*const btn = document.createElement("button");
       col.appendChild(btn);
       btn.addEventListener("click", event => {
+      event.preventDefault();
+      col.className = "d-none";*/
+    });
+    const bottoni = document.querySelectorAll(".bottoneCanc");
+    bottoni.forEach(bottone => {
+      bottone.addEventListener("click", event => {
         event.preventDefault();
-        col.className = "d-none";
+        document.querySelector(".col").className = "d-none";
       });
     });
   })
