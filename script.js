5f205cd5a2ab4ce50ac3ad17c2b5b2285ed59dcd
francescocaccia <123968277+francescocaccia@users.noreply.github.com>
2023-03-15 15:19:11 +0100

    homeWork

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 fetch("https://striveschool-api.herokuapp.com/books")
-  // fetch fa una HTTP Request, che richiederà un tempo indefinito per risolversi e ricevere la Response dal server
-  // quando la response arriva, fetch chiama internamente il metodo resolve()
-  // chiamare resolve in una Promise fa scattare il metodo .then() concatenato alla esecuzione della Promise
-
   .then(responseObj => responseObj.json())
-  // per ottenere dati usabili, dovremo convertire il reponseObj.body, che arriva come ReadableStream, in un elemento JS utilizzabile
-  // questo processo lo fa il metodo .json(), che è a sua volta una Promise che noi stiamo tornando implicitamente dalla nostra arrow function
   .then(books => {
-    // questa funzione verrà eseguita dal suo metodo .then() al momento opportuno, ovvero: quando la Promise corrispondente al metodo .json() si è risolta correttamente
-    // in caso contrario si salta direttamente, in caso di problemi, dentro al metodo .catch()
-
-    // se siamo qui possiamo dare per assodato che abbiamo i dati, in questo caso nel parametro di questa funzione chiamato todos (il body parsato)
-
     console.log(books);
 
     const grid = document.getElementById("books-container");
